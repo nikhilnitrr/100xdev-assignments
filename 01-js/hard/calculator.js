6f5515b18bacc5cb3ea9ef035ebc8912240992ab
Nikhil Chandrakar <nichandr@akamai.com>
2024-01-07 12:05:28 +0530

    Add solutions of hard problems

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,44 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+const math = require("mathjs")
+
+class Calculator {
+  constructor(){
+    this.result = 0;
+  }
+  add(num){
+    this.result += num;
+  }
+  subtract(num){
+    this.result -= num;
+  }
+  multiply(num){
+    this.result *= num;
+  }
+  divide(num){
+    if(num === 0){
+      throw new Error("Not divisible by 0");
+    }
+    this.result /= num;
+  }
+  clear(){
+    this.result = 0;
+  }
+  getResult(){
+    return this.result;
+  }
+  calculate(str){
+    try{
+      let value = math.evaluate(str);
+      if(!value || value == Infinity || value == -Infinity){
+        throw new Error("Invalid expression")
+      }
+      this.result = value;
+    }catch(err){
+      throw new Error(err)
+    }
+  }
+}
 
 module.exports = Calculator;
